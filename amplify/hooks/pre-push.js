3fc811e33cb7d681b36233d5a47bf1823267b2b4
Jainam Shah <jainam.2000@yahoo.com>
2021-11-16 12:23:59 -0800

    fixed: pre-push hook

--- amplify/hooks/pre-push.js
+++ amplify/hooks/pre-push.js
@@ -2,17 +2,19 @@
 const fs = require("fs");
 const projectRoot = process.cwd();
 const functionPath = projectRoot + "/amplify/backend/function";
-fs.readdirSync(projectRoot + "/amplify/backend/function").forEach(
-  (funcDirName) => {
-    if (fs.existsSync(functionPath + "/" + funcDirName + "/keys.json")) {
-      console.log(
-        "\nplease remove keys.json from " +
-          funcDirName +
-          " function and try to push again.\n"
-      );
-      throw Error("Found keys.json in " + funcDirName);
+if (os.path.isdir(functionPath)) {
+  fs.readdirSync(projectRoot + "/amplify/backend/function").forEach(
+    (funcDirName) => {
+      if (fs.existsSync(functionPath + "/" + funcDirName + "/keys.json")) {
+        console.log(
+          "\nplease remove keys.json from " +
+            funcDirName +
+            " function and try to push again.\n"
+        );
+        throw Error("Found keys.json in " + funcDirName);
+      }
     }
-  }
-);
+  );
+}
 
 console.log("all pre-push hooks checks passed successfully!");
